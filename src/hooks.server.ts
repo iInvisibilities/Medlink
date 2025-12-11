9547336c0cd2d6091e3e2cc4bf818abdeb84b019
iInvisibilities <abuserod@gmail.com>
2025-12-11 15:56:28 +0100

    undo cors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,11 +5,7 @@ export const handle: Handle = async ({ event, resolve }) => {
   const sessionId = event.cookies.get("medlink_session") ?? null;
   const session = await validateSession(sessionId);
   event.locals.session = session;
-  const response = await resolve(event);
-  response.headers.set('Access-Control-Allow-Origin', '*'); // for testing purposes, will be updated soon.
-  response.headers.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-  return response;
+  return resolve(event);
 };
 
 declare module "@sveltejs/kit" {
